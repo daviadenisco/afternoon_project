cda9d628a8b4226d1ea415217a8c89e03cd4a604
Davia DeNisco <davia622@gmail.com>
2018-03-15 15:42:11 -0400

    Add eslint and express
    Linted file and corrected problems.

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 const express = require('express');
-const PORT = process.env.PORT || 5150
+
+const PORT = process.env.PORT || 5150;
 const app = express();
 
 app.use(express.static('/public'));
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
   res.render('index');
-})
+});
 
-app.get('/about',(req,res)=>{
-  res.send('this is the about page')
-})
-app.get('/contact',(req,res)=>{
-  res.send('this is the contact page')
-})
-app.get('/subscribe',(req,res)=>{
-  res.send('Please subscribe to this page')
+app.get('/about', (req, res) => {
+  res.send('this is the about page');
+});
+app.get('/contact', (req, res) => {
+  res.send('this is the contact page');
+});
 
-})
-app.listen(PORT, ()=>{
+app.get('/subscribe', (req, res) => {
+  res.send('Please subscribe to this page');
+});
+
+app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
-})
+});
